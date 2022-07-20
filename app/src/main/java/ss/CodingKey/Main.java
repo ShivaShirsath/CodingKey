e8f46ed210bcfd309d67285738b4bdfa5e7d5c2a
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-07-20 23:47:48 +0530

    mc

--- app/src/main/java/ss/CodingKey/Main.java
+++ app/src/main/java/ss/CodingKey/Main.java
@@ -24,60 +24,27 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        String html = getString(R.string.main_body);
-        html += "<p><i>Version: " + getString(R.string.auto_version) + "</i></p>";
-        Spanned content = Html.fromHtml(html);
-        TextView description = (TextView) findViewById(R.id.main_description);
-        description.setMovementMethod(LinkMovementMethod.getInstance());
-        description.setText(content, BufferType.SPANNABLE);
-
-
+        
         final Button setup1 = (Button) findViewById(R.id.main_setup_btn_configure_imes);
         setup1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivityForResult(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
-            }
-        });
+                public void onClick(View v) {
+                    startActivityForResult(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
+                }
+            });
 
         final Button setup2 = (Button) findViewById(R.id.main_setup_btn_set_ime);
         setup2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                mgr.showInputMethodPicker();
-            }
-        });
-        
-        final Activity that = this;
-
-        final Button setup4 = (Button) findViewById(R.id.main_setup_btn_input_lang);
-        setup4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivityForResult(new Intent(that, InputLanguageSelection.class), 0);
-            }
-        });
-
-        final Button setup3 = (Button) findViewById(R.id.main_setup_btn_get_dicts);
-        setup3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URI));
-                try {
-                	startActivity(it);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(getApplicationContext(),
-                            getResources().getString(
-                            		R.string.no_market_warning), Toast.LENGTH_LONG)
-                            .show();
+                public void onClick(View v) {
+                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    mgr.showInputMethodPicker();
                 }
-            }
-        });
-        // PluginManager.getPluginDictionaries(getApplicationContext()); // why?
+            });
 
         final Button setup5 = (Button) findViewById(R.id.main_setup_btn_settings);
         setup5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivityForResult(new Intent(that, LatinIMESettings.class), 0);
-            }
-        });
+                public void onClick(View v) {
+                    startActivityForResult(new Intent(Main.this, LatinIMESettings.class), 0);
+                }
+            });
     }    
 }
-
